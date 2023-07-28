d3e3956fc118d6bbed78e3db74d42fd52665e4ea
balapavithra <pavikannan2991@gmail.com>
2023-07-28 20:10:45 +0530

    task 2 changed

--- task2.js
+++ task2.js
@@ -1,11 +1,10 @@
-let values = [[5, 8, 7, 12, 13], [1, 3, 4]];
-let delvalues=[];
+const values = [5, 8, 7, 12, 13];
 function deletedvalue() {
-let value = values.flat()
+    const remove = [1, 3, 4];
 
-let x = value.splice(0, 1);
-
-x.push(value.splice(1, 1))
-return delvalues = x.flat();
+    for (let i = 0; i < remove.length; i++) {
+        values.splice(remove[i], 1);
+        console.log(values);
+    }
 }
-console.log("Deleted values are: ",deletedvalue());
+deletedvalue();
